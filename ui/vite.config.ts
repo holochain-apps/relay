56847875b0a3446a34c744e123111c3c31d6b58a
Eric Harris-Braun <eric@harris-braun.com>
2024-05-31 10:58:07 -0400

    fixes

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -18,14 +18,6 @@ export default defineConfig({
 	build: {
     minify: false
   },
-  server: {
-    hmr: {
-        host: 'localhost',
-    },
-    watch: {
-        usePolling: true
-    }
-  },
   define: {
     "process.env.IS_PREACT": JSON.stringify("false"),
     "process.env.NODE_ENV": JSON.stringify("development")
