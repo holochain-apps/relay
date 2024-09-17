e3cd15f70f6c80b789fa5e8359d09845ecba8d36
guillem.cordoba <guillem.cordoba@gmail.com>
2024-09-17 12:38:57 +0200

    Proper handling of keystore

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -13,7 +13,7 @@ val tauriProperties = Properties().apply {
         propFile.inputStream().use { load(it) }
     }
 }
-val keystorePropertiesFile = rootProject.file("key.properties")
+val keystorePropertiesFile = rootProject.file("keystore.properties")
 val keystoreProperties = Properties()
 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
@@ -21,9 +21,9 @@ android {
     signingConfigs {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
+            keyPassword = keystoreProperties["password"] as String
             storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            storePassword = keystoreProperties["password"] as String
         }
     }
     compileSdk = 34
