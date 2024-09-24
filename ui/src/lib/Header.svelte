226a84e39fc04c7ed0c975209b47981d4cf870af
Tibet Sprague <tibet.sprague@gmail.com>
2024-09-24 11:03:10 -0700

    Align content of header bar better

--- ui/src/lib/Header.svelte
+++ ui/src/lib/Header.svelte
@@ -1,3 +1,3 @@
-<div class='flex flex-row flex-shrink-0 items-start w-full pt-4 px-4 relative'>
+<div class='flex flex-row flex-shrink-0 items-center w-full pt-4 px-4 relative'>
   <slot></slot>
 </div>
