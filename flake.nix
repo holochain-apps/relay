c36db44f1c676e92434cd36973ee0b1f49ccbf3e
Matt Gabrenya <matt@buildyourweb.app>
2024-10-12 12:19:39 -0400

    build: tauri deps require rust 1.79.0, so use that in nix flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
 
     nixpkgs.follows = "holonix/nixpkgs";
     flake-parts.follows = "holonix/flake-parts";
+    rust-overlay.follows = "holonix/rust-overlay";
 
     p2p-shipyard.url = "github:darksoil-studio/p2p-shipyard";
   };
@@ -14,17 +15,39 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = builtins.attrNames inputs.holonix.devShells;
       perSystem = { inputs', config, pkgs, system, ... }: {
-        devShells.default = pkgs.mkShell {
+        devShells.default = let
+          overlays = [ (import inputs.rust-overlay) ];
+          rustPkgs = import pkgs.path { inherit system overlays; };
+          rust = rustPkgs.rust-bin.stable."1.79.0".default;
+        in pkgs.mkShell {
           inputsFrom = [ 
             inputs'.p2p-shipyard.devShells.holochainTauriDev 
             inputs'.holonix.devShells.default
           ];
+          packages = [ rust ];
         };
-        devShells.androidDev = pkgs.mkShell {
+        devShells.androidDev = let
+          overlays = [ (import inputs.rust-overlay) ];
+          rustPkgs = import pkgs.path { inherit system overlays; };
+          rust = rustPkgs.rust-bin.stable."1.79.0".default.override {
+            targets = [
+              "armv7-linux-androideabi"
+              "x86_64-linux-android"
+              "i686-linux-android"
+              "aarch64-unknown-linux-musl"
+              "wasm32-unknown-unknown"
+              "x86_64-pc-windows-gnu"
+              "x86_64-unknown-linux-musl"
+              "x86_64-apple-darwin"
+              "aarch64-linux-android"
+            ];
+          };
+        in pkgs.mkShell {
           inputsFrom = [ 
             inputs'.p2p-shipyard.devShells.holochainTauriAndroidDev 
             inputs'.holonix.devShells.default
           ];
+          packages = [ rust ];
         };
       };
     };
