d6e85c27ded7f3802a94d3886a4d04b0da7f9097
Matt Gabrenya <matt@buildyourweb.app>
2024-10-16 17:26:05 -0400

    feat(ui): display error on scan page if camera permissions not granted, display button to open app settings -- requesting permissions every time not working :(

--- ui/src/routes/scan/+page.svelte
+++ ui/src/routes/scan/+page.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
-  import { scan, cancel, checkPermissions, requestPermissions, Format } from '@tauri-apps/plugin-barcode-scanner'
+  import { scan, cancel, checkPermissions, requestPermissions, openAppSettings,  Format } from '@tauri-apps/plugin-barcode-scanner'
   import Button from '$lib/Button.svelte';
   import { t } from '$lib/translations';
   import { scanStore } from '$store/ScanStore';
   import { setBackgroundColor } from '$lib/background';
-  
+  import { onDestroy, onMount } from 'svelte';
+  import SvgIcon from '$lib/SvgIcon.svelte';
+
   let needsPermission = false;
 
   $: isSupported = scanStore.isSupported;
 
   async function ensurePermissions() {
-    const permissionsState = await checkPermissions();
-    if(permissionsState === "prompt") {
-      try {
-        await requestPermissions();
-      } catch(e) {
-        console.error(e);
-        needsPermission = true;
-      }
-    } else {
-      needsPermission = true;
+    let permissionsState = await checkPermissions();
+    if(permissionsState === "granted") return;
+
+    try {
+      permissionsState = await requestPermissions();
+      if(permissionsState === "granted") return;
+    } catch(e) {
+      console.error('requestPermissions error', e);
     }
+
+    needsPermission = true;
   }
 
   async function executeScan() {
@@ -28,13 +30,12 @@
       const res = await scan({ windowed: true, formats: [Format.QRCode] });
       scanSuccess(res.content);
     } catch (e) {
-      console.error(e);
+      console.error('executeScan error', e);
     }
   }
   
   function scanSuccess(value: string) {
     scanStore.value.set(value);
-    resetBackgroundColor();
     scanStore.complete();
   }
 
@@ -45,8 +46,6 @@
     } catch(e) {
       console.error(e);
     }
-
-    resetBackgroundColor();
     scanStore.complete();
   }
 
@@ -55,8 +54,16 @@
     await executeScan();
   }
 
+
   load();
+
+  // tauri-plugin-barcode-scanner is launched in its own View BEHIND the current webview
+  // Thus we must set the body background color to transparent for this page only,
+  // to expose the View behind it.
   let resetBackgroundColor = setBackgroundColor('transparent');
+  onDestroy(() => {
+    resetBackgroundColor();
+  });
 </script>
 
 {#if $isSupported}
@@ -64,16 +71,40 @@
     <div class="flex flex-col w-screen h-screen justify-center items-center">
       
       <!-- Scanning Area -->
-      <div class="w-64 h-64 border-solid border-white border-2 rounded-lg shadow-full"></div>
+      <div class="w-64 h-64 border-solid border-white border-2 rounded-lg shadow-full">
+        {#if needsPermission}
+          <div class="h-full w-full flex justify-center items-center">
+            <SvgIcon icon='warning' color="%232e2e2e" size='120' />
+          </div>
+        {/if}
+      </div>
 
       <!-- Action Buttons -->
-      <div class="my-8">
+      {#if needsPermission}
+        <div class="mt-4">
+          <p class='text-error-500 text-sm mt-2'>{$t('common.need_camera_permissions')}</p>
+        </div>
+      {/if}
+
+      <div class="my-8 flex justify-center items-center space-x-4">
         <Button
           moreClasses='w-36 justify-center !variant-filled-tertiary dark:!variant-filled-secondary'
           onClick={() => scanCancel()}
         >
           <strong>{$t('common.cancel')}</strong>
         </Button>
+
+        {#if needsPermission}
+          <Button
+            moreClasses='w-36 justify-center !variant-filled-tertiary dark:!variant-filled-secondary'
+            onClick={() => {
+              scanCancel();
+              openAppSettings();
+            }}
+          >
+            <strong>{$t('common.open_app_settings')}</strong>
+          </Button>
+        {/if}
       </div>
     </div>
   </div>
