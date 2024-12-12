396c18d986dcc58e39cc030c6e3938b315bc18ac
Keshav Agarwal <64908981+MasterK0927@users.noreply.github.com>
2024-12-12 22:16:30 +0530

    Merge pull request #304 from holochain-apps/feat/success-error-notices
    fix/submit button disabled for firstname length greater than min length

--- ui/src/routes/account/+page.svelte
+++ ui/src/routes/account/+page.svelte
@@ -1,32 +1,40 @@
 <script lang="ts">
-  import { modeCurrent } from "@skeletonlabs/skeleton";
-  import { getContext, onMount } from "svelte";
-  import { QRCodeImage } from "svelte-qrcode-image";
-  import Avatar from "$lib/Avatar.svelte";
-  import Button from "$lib/Button.svelte";
-  import Header from "$lib/Header.svelte";
-  import SvgIcon from "$lib/SvgIcon.svelte";
-  import { t } from "$lib/translations";
-  import { copyToClipboard, handleFileChange, isMobile, shareText } from "$lib/utils";
-  import { RelayClient } from "$store/RelayClient";
-  import { ProfilesStore } from "@holochain-open-dev/profiles";
-  import { get } from "svelte/store";
-  import toast from "svelte-french-toast";
+  import { modeCurrent } from '@skeletonlabs/skeleton';
+  import { getContext, onMount } from 'svelte';
+  import { QRCodeImage } from 'svelte-qrcode-image';
+  import Avatar from '$lib/Avatar.svelte';
+  import Button from '$lib/Button.svelte';
+  import Header from '$lib/Header.svelte';
+  import SvgIcon from '$lib/SvgIcon.svelte';
+  import { t } from '$lib/translations';
+  import {
+    copyToClipboard,
+    handleFileChange,
+    isMobile,
+    shareText,
+  } from '$lib/utils';
+  import { RelayClient } from '$store/RelayClient';
+  import { ProfilesStore } from '@holochain-open-dev/profiles';
+  import { get } from 'svelte/store';
+  import toast from 'svelte-french-toast';
 
-  const relayClientContext: { getClient: () => RelayClient } = getContext("relayClient");
+  const relayClientContext: { getClient: () => RelayClient } =
+    getContext('relayClient');
   let relayClient = relayClientContext.getClient();
 
-  const profilesContext: { getStore: () => ProfilesStore } = getContext("profiles");
+  const profilesContext: { getStore: () => ProfilesStore } =
+    getContext('profiles');
   let profilesStore = profilesContext.getStore();
   $: prof = profilesStore ? profilesStore.myProfile : undefined;
-  $: profileData = $prof?.status === "complete" ? $prof.value?.entry : undefined;
+  $: profileData =
+    $prof?.status === 'complete' ? $prof.value?.entry : undefined;
 
   const agentPublicKey64 = relayClient.myPubKeyB64;
 
   const MIN_FIRST_NAME_LENGTH = 3;
 
-  $: firstName = profileData?.fields.firstName || "";
-  $: lastName = profileData?.fields.lastName || "";
+  $: firstName = profileData?.fields.firstName || '';
+  $: lastName = profileData?.fields.lastName || '';
   $: isFirstNameValid = firstName.trim().length >= MIN_FIRST_NAME_LENGTH;
 
   let editingName = false;
@@ -35,18 +43,25 @@
 
   $: saveName = async () => {
     if (profileData && firstNameElem.value?.length >= MIN_FIRST_NAME_LENGTH) {
-      firstName = firstNameElem.value;
-      lastName = lastNameElem.value;
-      await relayClient.updateProfile(firstName, lastName, profileData.fields.avatar);
-      editingName = false;
+      try {
+        const firstName = firstNameElem.value;
+        const lastName = lastNameElem.value;
+        await relayClient.updateProfile(
+          firstName,
+          lastName,
+          profileData.fields.avatar,
+        );
+        editingName = false;
+      } catch (e) {
+        toast.error(`${$t('common.update_profile_error')}: ${e.message}`);
+      }
     }
-    toast.error(`${$t("common.update_profile_error")}: ${e.message}`);
   };
 
   $: cancelEditName = () => {
     editingName = false;
-    firstName = profileData?.fields.firstName || "";
-    lastName = profileData?.fields.lastName || "";
+    firstName = profileData?.fields.firstName || '';
+    lastName = profileData?.fields.lastName || '';
   };
 
   onMount(() => {
@@ -57,11 +72,15 @@
 
 <Header>
   <button class="pr-5 text-4xl" on:click={() => history.back()}
-    ><SvgIcon icon="caretLeft" color={$modeCurrent ? "%232e2e2e" : "white"} size="10" /></button
+    ><SvgIcon
+      icon="caretLeft"
+      color={$modeCurrent ? '%232e2e2e' : 'white'}
+      size="10"
+    /></button
   >
 </Header>
 
-{#if $prof && $prof.status === "complete" && $prof.value}
+{#if $prof && $prof.status === 'complete' && $prof.value}
   <div class="flex w-full grow flex-col items-center pt-10">
     <!-- Hidden file input -->
     <input
@@ -75,17 +94,25 @@
             relayClient.updateProfile(firstName, lastName, imageData);
           });
         } catch (e) {
-          toast.error(`${$t("common.upload_image_error")}: ${e.message}`);
+          toast.error(`${$t('common.upload_image_error')}: ${e.message}`);
         }
       }}
     />
     <div style="position:relative">
-      <Avatar agentPubKey={relayClient.myPubKey} size="128" moreClasses="mb-4" />
+      <Avatar
+        agentPubKey={relayClient.myPubKey}
+        size="128"
+        moreClasses="mb-4"
+      />
       <label
         for="avatarInput"
         class="absolute bottom-5 right-0 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-tertiary-500 pl-1 hover:bg-secondary-300 dark:bg-secondary-500 dark:hover:bg-secondary-400"
       >
-        <SvgIcon icon="image" color={$modeCurrent ? "%232e2e2e" : "white"} size="26" />
+        <SvgIcon
+          icon="image"
+          color={$modeCurrent ? '%232e2e2e' : 'white'}
+          size="26"
+        />
       </label>
     </div>
 
@@ -95,25 +122,25 @@
           autofocus
           class="text-3xl max-w-40 text-start bg-surface-900 border-none outline-none focus:outline-none pl-0.5 pt-0 focus:ring-0"
           type="text"
-          placeholder={$t("common.first") + " *"}
+          placeholder={$t('common.first') + ' *'}
           name="firstName"
           bind:this={firstNameElem}
           value={firstName}
           minlength={MIN_FIRST_NAME_LENGTH}
           on:keydown={(event) => {
-            if (event.key === "Escape") cancelEditName();
+            if (event.key === 'Escape') cancelEditName();
           }}
         />
         <input
           class="text-3xl max-w-40 text-start bg-surface-900 border-none outline-none focus:outline-none pl-0.5 pt-0 focus:ring-0"
           type="text"
-          placeholder={$t("common.last")}
+          placeholder={$t('common.last')}
           name="lastName"
           bind:this={lastNameElem}
           value={lastName}
           on:keydown={(event) => {
-            if (event.key === "Enter") saveName();
-            if (event.key === "Escape") cancelEditName();
+            if (event.key === 'Enter') saveName();
+            if (event.key === 'Escape') cancelEditName();
           }}
         />
         <Button
@@ -135,7 +162,12 @@
         <h1 class="text-3xl flex-shrink-0 mr-2">{firstName} {lastName}</h1>
 
         <button on:click={() => (editingName = true)}>
-          <SvgIcon icon="write" size="24" color="gray" moreClasses="cursor-pointer" />
+          <SvgIcon
+            icon="write"
+            size="24"
+            color="gray"
+            moreClasses="cursor-pointer"
+          />
         </button>
       </div>
     {/if}
@@ -152,15 +184,15 @@
       onClick={async () => {
         try {
           await copyToClipboard(agentPublicKey64);
-          toast.success(`${$t("common.copy_success")}`);
+          toast.success(`${$t('common.copy_success')}`);
         } catch (e) {
-          toast.error(`${$t("common.copy_error")}: ${e.message}`);
+          toast.error(`${$t('common.copy_error')}: ${e.message}`);
         }
       }}
       moreClasses="w-64 text-sm variant-filled-tertiary dark:!bg-tertiary-200"
     >
       <SvgIcon icon="copy" size="22" color="%23FD3524" moreClasses="mr-3" />
-      <strong>{$t("common.copy_your_contact_code")}</strong>
+      <strong>{$t('common.copy_your_contact_code')}</strong>
     </Button>
     {#if isMobile()}
       <Button
@@ -168,13 +200,13 @@
           try {
             await shareText(agentPublicKey64);
           } catch (e) {
-            toast.error(`${$t("common.share_code_error")}: ${e.message}`);
+            toast.error(`${$t('common.share_code_error')}: ${e.message}`);
           }
         }}
         moreClasses="w-64 text-sm variant-filled-tertiary dark:!bg-tertiary-200"
       >
         <SvgIcon icon="share" size="22" color="%23FD3524" moreClasses="mr-3" />
-        <strong>{$t("common.share_your_contact_code")}</strong>
+        <strong>{$t('common.share_your_contact_code')}</strong>
       </Button>
     {/if}
   </div>
